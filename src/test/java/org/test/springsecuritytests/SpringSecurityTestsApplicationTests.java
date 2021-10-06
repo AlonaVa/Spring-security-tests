7aea4cd45eb88d4fe456cb97e90d046032865680
Alona Vasylchenko <alona.vasylchenko@avast.com>
2021-10-06 14:48:35 +0200

    Create an Unsecured Web Application

---
+++ src/test/java/org/test/springsecuritytests/SpringSecurityTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.springsecuritytests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
