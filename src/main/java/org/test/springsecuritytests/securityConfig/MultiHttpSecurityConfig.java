976953abc1cfcc3800e0e4f1620f34010aeb2791
Alona Vasylchenko <alona.vasylchenko@avast.com>
2021-10-27 14:36:35 +0200

    Set up Spring Security configure httpBasic() and formLogin()

--- src/main/java/org/test/springsecuritytests/securityConfig/MultiHttpSecurityConfig.java
+++ src/main/java/org/test/springsecuritytests/securityConfig/MultiHttpSecurityConfig.java
@@ -2,28 +2,14 @@ package org.test.springsecuritytests.securityConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 @Configuration
@@ -46,61 +32,20 @@ public class MultiHttpSecurityConfig {
             http
                     .authorizeRequests()
                     .antMatchers("/", "/home", "/login").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
+                    .anyRequest().authenticated().and()
                     .httpBasic()
-
-
-                    .and().exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint());
+                    .and()
+                    .formLogin()
+                    .loginPage("/login");
         }
     }
 
-    @Bean
-    public static AuthenticationEntryPoint unauthorizedEntryPoint() {
-        return (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-    }
 
     @Bean
-    public AuthenticationFailureHandler handleAuthenticationFailure() {
-        return new SimpleUrlAuthenticationFailureHandler() {
-
-            @Override
-            public void onAuthenticationFailure(HttpServletRequest httpRequest, HttpServletResponse httpResponse,
-                                                AuthenticationException authenticationException) throws IOException, ServletException {
-
-                // custom failure code here
-                setDefaultFailureUrl("/login");
-                super.onAuthenticationFailure(httpRequest, httpResponse, authenticationException);
-            }
-        };}
-
-        @Configuration
-        @Order(2)
-        public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-//        @Override
-//        public void configure(WebSecurity web) throws Exception {
-//            web
-//                    .ignoring()
-//                    .antMatchers("/resources/**");
-//        }
-
-            @Override
-            protected void configure(HttpSecurity http) throws Exception {
-                http
-                        .authorizeRequests()
-                        .antMatchers("/", "/home").permitAll()
-                        .anyRequest().hasRole("USER")
-                        .and()
-                        .formLogin()
-                        .loginPage("/login")
-                        .permitAll();
-            }
-
-            @Bean
-            public PasswordEncoder passwordEncoder() {
-                return NoOpPasswordEncoder.getInstance();
-            }
-        }
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
+}
+
 
 
